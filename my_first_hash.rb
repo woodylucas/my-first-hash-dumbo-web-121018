5604243a090dc9fc050772dd4e4a498e61e3160d
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 04:24:12 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,6 +24,6 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
    shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
-  shipping_manifest["pearl necklace"] = 1
+  
   shipping_manifest
 end 
