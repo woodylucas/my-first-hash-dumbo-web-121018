0d9833dd3e21f5a9bbb3c4665c645a957e6539f9
Woody Lucas <wlucas0311@gmail.com>
2018-11-27 04:22:11 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,45 +1,29 @@
 def my_hash
-  person = {"name" => "Sade", "age" => 26}
+  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"name" => "Sade", "age" => 25, "location" => "NY"}
+end 
 
+ #set a variable called `shipping_manifest`, equal to a hash	  #set a variable called `shipping_manifest`, equal to a hash
+  #fill that hash with key/value pairs that describe the following information: 	  #fill that hash with key/value pairs that describe the following information: 
+  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   
-end
-
-
-def shipping_manifest
-  the_manifest = {
-    "whale bone corsets" => 5,
-    "procelain vases" => 2, 
-    "oil paintings" => 3  
-  }
-  
-
+  def shipping_manifest
+    shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  
+  #your code here!
+  
+  shipping_manifest["oil paintings"]
+  
+end 
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-shipping_manifest
-end
+def adding 
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+   shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
+end 
